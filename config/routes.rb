38231be00f63b73a07b137ca63e4a49c3a422361
MwangiJim <kingongomwangi@gmail.com>
2023-10-17 12:42:15 +0100

    added associations between user and friends

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :friends
   #get 'home/index'
   get 'home/about'
-  root 'home#index'
+  #root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
